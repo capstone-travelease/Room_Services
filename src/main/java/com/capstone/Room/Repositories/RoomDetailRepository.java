ae400dfeb6903bb27e00b68c10a9282154e72bd4
billzayy <coderbillzay@gmail.com>
2024-04-25 23:59:38 +0700

    refactor: modify repository room detail query

--- src/main/java/com/capstone/Room/Repositories/RoomDetailRepository.java
+++ src/main/java/com/capstone/Room/Repositories/RoomDetailRepository.java
@@ -13,9 +13,9 @@ import java.util.List;
 @Repository
 public interface RoomDetailRepository extends JpaRepository<Roomdetail, Long> {
     @Query("SELECT new com.capstone.Room.Entities.ResponseRoomDetail(r.room_name, rd.room_id, room_description, rd.room_size, rd.room_bed_quantity, rd.room_capacity, r.room_quantity, r.room_price) FROM Roomdetail rd \n" +
-            "INNER JOIN RoomAttachment rat ON rat.room_id = rd.room_detail_id\n" +
-            "INNER JOIN Attachment atc ON atc.attachment_id = rat.attachmentId\n" +
             "INNER JOIN Rooms r ON r.room_id = rd.room_id\n" +
+            "INNER JOIN RoomAttachment rat ON rat.room_id = r.room_id\n" +
+            "INNER JOIN Attachment atc ON atc.attachment_id = rat.attachmentId\n" +
             "INNER JOIN RoomFacilities rf ON rf.room_id = rd.room_detail_id\n" +
             "INNER JOIN Facilities fac ON fac.facility_id = rf.facility_id\n" +
             "WHERE r.room_id = ?1")
